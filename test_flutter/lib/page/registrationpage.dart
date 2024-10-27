0756ccfc24b92dabe29d3b7ddde40f6a27556dfc
NSnirjash <158472502+NSnirjash@users.noreply.github.com>
2024-10-27 18:42:34 +0600

    complete RP

--- test_flutter/lib/page/registrationpage.dart
+++ test_flutter/lib/page/registrationpage.dart
@@ -1,70 +1,66 @@
 import 'dart:convert';
-
-import 'package:date_field/date_field.dart';
 import 'package:flutter/material.dart';
-import 'package:dob_input_field/dob_input_field.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 import 'package:test_flutter/page/loginpage.dart';
 
 class RegistrationPage extends StatefulWidget {
-
   @override
   State<RegistrationPage> createState() => _RegistrationPageState();
 }
 
 class _RegistrationPageState extends State<RegistrationPage> {
   final TextEditingController name = TextEditingController();
-
   final TextEditingController email = TextEditingController();
-
   final TextEditingController password = TextEditingController();
-
   final TextEditingController confirmPassword = TextEditingController();
-
-  final TextEditingController cell = TextEditingController();
-
+  final TextEditingController phone = TextEditingController();
   final TextEditingController address = TextEditingController();
 
-  final DateTimeFieldPickerPlatform dob = DateTimeFieldPickerPlatform.material;
-
-  String? selectedGender;
-  DateTime? selectedDOB;
-
+  String? selectedRole;
   final _formKey = GlobalKey<FormState>();
-
-  // void _register() {
-  //   if (_formKey.currentState!.validate()) {
-  //
-  //     String uName = name.text;
-  //     String uEmail = email.text;
-  //     String uPassword = password.text;
-  //
-  //     print('Name: $uName, Email: $uEmail, Password: $uPassword');
-  //   }
-  // }
+  bool isLoading = false;
 
   void _register() async {
-
     if (_formKey.currentState!.validate()) {
       String uName = name.text;
       String uEmail = email.text;
       String uPassword = password.text;
-      String uCell = cell.text;
+      String uPhone = phone.text;
       String uAddress = address.text;
-      String uGender = selectedGender ?? 'Other';
-      String uDob = selectedDOB != null ? selectedDOB!.toIso8601String() : '';
+      String uRole = selectedRole ?? '';
 
-      // Send data to the server
-      final response = await _sendDataToBackend(uName, uEmail, uPassword, uCell, uAddress, uGender, uDob);
+      if (uRole.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Please select a role')),
+        );
+        return;
+      }
 
-      if (response.statusCode == 201 || response.statusCode == 200  ) {
-        // Registration successful
+      setState(() {
+        isLoading = true;
+      });
+
+      final response = await _sendDataToBackend(
+        uName, uEmail, uPassword, uPhone, uAddress, uRole,
+      );
+
+      setState(() {
+        isLoading = false;
+      });
+
+      if (response.statusCode == 201 || response.statusCode == 200) {
         print('Registration successful!');
+        // Optionally navigate or show a success message
       } else if (response.statusCode == 409) {
-        // User already exists
         print('User already exists!');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('User already exists!')),
+        );
       } else {
         print('Registration failed with status: ${response.statusCode}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Registration failed! Please try again.')),
+        );
       }
     }
   }
@@ -73,12 +69,10 @@ class _RegistrationPageState extends State<RegistrationPage> {
       String name,
       String email,
       String password,
-      String cell,
+      String phone,
       String address,
-      String gender,
-      String dob,
+      String role,
       ) async {
-
     const String url = 'http://localhost:8090/register'; // Android emulator
     final response = await http.post(
       Uri.parse(url),
@@ -87,10 +81,9 @@ class _RegistrationPageState extends State<RegistrationPage> {
         'name': name,
         'email': email,
         'password': password,
-        'cell': cell,
+        'phone': phone,
         'address': address,
-        'gender': gender,
-        'dob': dob,
+        'role': role,
       }),
     );
     return response;
@@ -100,207 +93,202 @@ class _RegistrationPageState extends State<RegistrationPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.only(top: 20, bottom: 20, right: 40, left: 40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Registration Form",
-              style: TextStyle(
-                fontSize: 40,
-                color: Colors.blue,
-              ),
-            ),
-
-            const SizedBox(
-              height: 40,
-            ),
-
-            TextField(
-              controller: name,
-              decoration: InputDecoration(
-                label: Text("Full Name"),
-                border: UnderlineInputBorder(),
-                prefixIcon: Icon(Icons.person),
-              ),
-            ),
-
-            const SizedBox(
-              height: 20,
-            ),
-
-            TextField(
-              controller: email,
-              decoration: InputDecoration(
-                label: Text("Email"),
-                border: UnderlineInputBorder(),
-                prefixIcon: Icon(Icons.email),
-              ),
-            ),
-
-            const SizedBox(
-              height: 20,
-            ),
-
-            TextField(
-              controller: password,
-              decoration: InputDecoration(
-                label: Text("Password"),
-                border: UnderlineInputBorder(),
-                prefixIcon: Icon(Icons.password),
-              ),
-            ),
-
-            const SizedBox(
-              height: 20,
-            ),
-
-            TextField(
-              controller: confirmPassword,
-              decoration: InputDecoration(
-                label: Text("Rewrite Password"),
-                border: UnderlineInputBorder(),
-                prefixIcon: Icon(Icons.password),
-              ),
-            ),
-
-            const SizedBox(
-              height: 20,
-            ),
-
-            TextField(
-              controller: cell,
-              decoration: InputDecoration(
-                label: Text("Contact Number"),
-                border: UnderlineInputBorder(),
-                prefixIcon: Icon(Icons.phone),
-              ),
-            ),
-
-            const SizedBox(
-              height: 20,
-            ),
-
-            TextField(
-              controller: address,
-              decoration: InputDecoration(
-                label: Text("Permenent Address"),
-                border: UnderlineInputBorder(),
-                prefixIcon: Icon(Icons.home),
-              ),
-            ),
-
-            const SizedBox(
-              height: 20,
-            ),
-
-            DateTimeFormField(
-              decoration: const InputDecoration(labelText: 'Date of Birth'),
-              mode: DateTimeFieldPickerMode.date,
-              pickerPlatform: dob,
-              onChanged: (DateTime? value) {
-                print(value);
-              },
-            ),
-
-            const SizedBox(
-              height: 20,
-            ),
-
-            Row(
+        padding: EdgeInsets.all(40),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('Gender:'),
-                Expanded(
-                  child: Row(
-                    children: [
-                      Radio<String>(
-                        value: 'Male',
-                        groupValue: selectedGender,
-                        onChanged: (String? value) {
-                          setState(() {
-                            selectedGender = value;
-                          });
-                        },
-                      ),
-                      Text('Male'),
-                    ],
-                  ),
+                Text(
+                  "Registration Form",
+                  style: TextStyle(fontSize: 40, color: Colors.blue),
                 ),
-                Expanded(
-                  child: Row(
-                    children: [
-                      Radio<String>(
-                        value: 'Female',
-                        groupValue: selectedGender,
-                        onChanged: (String? value) {
-                          setState(() {
-                            selectedGender = value;
-                          });
-                        },
-                      ),
-                      Text('Female'),
-                    ],
+                const SizedBox(height: 40),
+                TextFormField(
+                  controller: name,
+                  decoration: InputDecoration(
+                    label: Text("Full Name"),
+                    border: UnderlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
                   ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null; // No error
+                  },
                 ),
-                Expanded(
-                  child: Row(
-                    children: [
-                      Radio<String>(
-                        value: 'Other',
-                        groupValue: selectedGender,
-                        onChanged: (String? value) {
-                          setState((){
-                            selectedGender = value;
-                          });
-                        },
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: email,
+                  decoration: InputDecoration(
+                    label: Text("Email"),
+                    border: UnderlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                      return 'Please enter a valid email';
+                    }
+                    return null; // No error
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: password,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    label: Text("Password"),
+                    border: UnderlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    return null; // No error
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: confirmPassword,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    label: Text("Rewrite Password"),
+                    border: UnderlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please confirm your password';
+                    }
+                    if (value != password.text) {
+                      return 'Passwords do not match';
+                    }
+                    return null; // No error
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: phone,
+                  decoration: InputDecoration(
+                    label: Text("Contact Number"),
+                    border: UnderlineInputBorder(),
+                    prefixIcon: Icon(Icons.phone),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your contact number';
+                    }
+                    return null; // No error
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: address,
+                  decoration: InputDecoration(
+                    label: Text("Permanent Address"),
+                    border: UnderlineInputBorder(),
+                    prefixIcon: Icon(Icons.home),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your address';
+                    }
+                    return null; // No error
+                  },
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text('Role:'),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Radio<String>(
+                            value: 'ADMIN',
+                            groupValue: selectedRole,
+                            onChanged: (String? value) {
+                              setState(() {
+                                selectedRole = value;
+                              });
+                            },
+                          ),
+                          Text('Admin'),
+                        ],
                       ),
-                      Text('Other'),
-                    ],
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Radio<String>(
+                            value: 'WAITER',
+                            groupValue: selectedRole,
+                            onChanged: (String? value) {
+                              setState(() {
+                                selectedRole = value;
+                              });
+                            },
+                          ),
+                          Text('Waiter'),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Radio<String>(
+                            value: 'USER',
+                            groupValue: selectedRole,
+                            onChanged: (String? value) {
+                              setState(() {
+                                selectedRole = value;
+                              });
+                            },
+                          ),
+                          Text('User'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                isLoading
+                    ? CircularProgressIndicator()
+                    : ElevatedButton(
+                  onPressed: _register,
+                  child: Text(
+                    "Register",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                        fontSize: 14),
+                  ),
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                ),
+                SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                    ),
                   ),
                 ),
               ],
             ),
-
-            const SizedBox(
-              height: 20,
-            ),
-
-            ElevatedButton(
-              onPressed: (){
-                _register();
-              },
-              child: Text("Registration",
-                style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
-                    fontSize: 14
-                ),
-              ),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue
-              ),
-            ),
-
-            SizedBox(
-                height: 20
-            ),
-
-            // Login Text Button
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-              child: Text(
-                'Login',
-                style: TextStyle(
-                  color: Colors.blue,
-                  decoration: TextDecoration.underline,
-                ),
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
